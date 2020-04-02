27e25b8052c95f18fa6f21849829a354a80717a1
Yancy <yanzhao.zhang1@ucalgary.ca>
2020-04-01 21:37:04 -0600

    add order ui and display order

--- FoodyFrontend/src/app/order-item/order-item.component.ts
+++ FoodyFrontend/src/app/order-item/order-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { OrderService } from '../service/order.service';
 
 @Component({
   selector: 'app-order-item',
@@ -7,11 +8,40 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class OrderItemComponent implements OnInit {
 
-  @Input() order: any;
+  order = {
+    id: -1,
+    userId: '',
+    name: '',
+    item: '',
+  }
+  orders = null;
 
+  @Output() orderInShow: any;
+  @Output() orderId: number;
+  @Output() userId: number;
 
-  constructor() { }
+  constructor(public orderService: OrderService) {
+    orderService.getOrdersByUser(this.orderId).subscribe(ord =>{
+      this.orders = ord;
+      this.orders.forEach(ord =>{
+        ord.items = [];
+      });
+      this.orderInShow = this.order[0];
+      orderService.getOrderItemsByUser(this.orderId).subscribe(items =>{
+        items.forEach(item => {
+          const or = this.orders.find(ord => item.ordID === ord.id);
+          if(or){
+            or.items.push(item);
+          }
+        });
+      });
+    });
+   }
 
   ngOnInit() {}
+
+  orderClick(order) {
+    this.orderInShow = order;
+  }
 
 }
