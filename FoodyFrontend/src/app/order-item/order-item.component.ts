22246384ee1752494df80169d06ce3aeec283f5e
Haomin <haomin.yu@ucalgary.ca>
2020-04-02 00:49:47 -0600

    update user order

--- FoodyFrontend/src/app/order-item/order-item.component.ts
+++ FoodyFrontend/src/app/order-item/order-item.component.ts
@@ -8,40 +8,10 @@ import { OrderService } from '../service/order.service';
 })
 export class OrderItemComponent implements OnInit {
 
-  order = {
-    id: -1,
-    userId: '',
-    name: '',
-    item: '',
-  }
-  orders = null;
-
-  @Output() orderInShow: any;
-  @Output() orderId: number;
-  @Output() userId: number;
+  @Input()order: any;
 
   constructor(public orderService: OrderService) {
-    orderService.getOrdersByUser(this.orderId).subscribe(ord =>{
-      this.orders = ord;
-      this.orders.forEach(ord =>{
-        ord.items = [];
-      });
-      this.orderInShow = this.order[0];
-      orderService.getOrderItemsByUser(this.orderId).subscribe(items =>{
-        items.forEach(item => {
-          const or = this.orders.find(ord => item.ordID === ord.id);
-          if(or){
-            or.items.push(item);
-          }
-        });
-      });
-    });
    }
 
   ngOnInit() {}
-
-  orderClick(order) {
-    this.orderInShow = order;
-  }
-
 }
